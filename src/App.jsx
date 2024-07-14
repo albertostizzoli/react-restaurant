23641f9357a833807c0c0367eca028033e900446
albertostizzoli <albertostizzoli@yahoo.it>
2024-07-14 16:34:46 +0200

    general CSS, cons and other test React

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,11 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-    <h1 className="text-center text-danger"> Ciao React</h1>
+    <h1 className="text-center text-bg-danger"> Ciao React<i class="fa-solid fa-bars"></i></h1>
+    
     </>
   )
 }
