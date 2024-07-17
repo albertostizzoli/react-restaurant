3b980826f84ab56f0f8cf1fabd1308481556414f
albertostizzoli <albertostizzoli@yahoo.it>
2024-07-17 14:59:38 +0200

    create sidebar component

--- src/components/navbar/NavbarComponent.jsx
+++ src/components/navbar/NavbarComponent.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import './navbar.css';
 import { motion } from "framer-motion";
+import Sidebar from "../sidebar/SidebarComponent";
 
 
 function Navbar() {
     return (
         <div className="navbar">
+            <Sidebar></Sidebar>
             <div className="wrapper">
                 <motion.span initial={{ opacity: 0, y: -100 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 1 }}>RISTORANTE PEPPERONE</motion.span>
                 <div className="social">
---
+++ src/components/sidebar/SidebarComponent.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { useState } from "react";
+import { motion } from "framer-motion";
+import './sidebar.css';
+
+
+function Sidebar(){
+
+    const [open, setOpen] = useState(false)
+
+    const variants = {
+        open:{
+            clipPath: 'circle(1200px at 50px 50px)',
+            transition: {
+                type: 'spring',
+                stiffness: 20,
+            }
+        },
+        closed: {
+            clipPath: 'circle(30px at 50px 50px)',
+            transition: {
+                delay: 0.5,
+                type: 'spring',
+                stiffness: 400,
+                damping: 40
+            }
+        }
+    }
+
+    return(
+        <motion.div className="sidebar" animate={open ? 'open' : 'closed'}>
+            <motion.div className="bg" variants={variants}>
+                
+            </motion.div>
+        </motion.div>
+        
+    )
+}
+
+export default Sidebar
