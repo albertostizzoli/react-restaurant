99b3ee29695cfcf9f6b33d9b178a3deaaa4b4bb7
albertostizzoli <albertostizzoli@yahoo.it>
2024-07-23 15:44:10 +0200

    adeed title Menu

--- src/components/menu/MenuComponent.jsx
+++ src/components/menu/MenuComponent.jsx
@@ -2,9 +2,25 @@ import React from "react";
 import { motion } from "framer-motion";
 import './menu.css';
 
-function Menu(){
-    return(
-         <div></div>
+function Menu() {
+
+    const title = {
+        initial: {
+            opacity: 0,
+            y: -100
+        },
+        animate: {
+            opacity: 1,
+            y: 0,
+            transition: {
+                duration: 1
+            }
+        }
+    }
+    return (
+        <div className="menu">
+            <motion.h2 initial="initial" whileInView={animate} variants={title}>IL NOSTRO MENU</motion.h2>
+        </div>
     )
 }
 
