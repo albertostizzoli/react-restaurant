30d213b9c77b463ce549b197482cef3fe7a3703f
albertostizzoli <albertostizzoli@yahoo.it>
2024-07-24 16:24:03 +0200

    create buttons for menu

--- src/components/menu/cards/CardComponent.jsx
+++ src/components/menu/cards/CardComponent.jsx
@@ -13,22 +13,21 @@ function Card() {
         fetch('src/menu.json').then(response => response.json()).then(data => { setData(data) })
     }, [])
 
-    const buttonsVariants = {
+    const firstButton = {
         selected: {
             backgroundColor: "var(--orange)",
             color: "white"
         },
         unselected: {
-            backgroundColor: "var(--main)",
-            color: "var(--white)",
+            backgroundColor: "var(--yellow)",
+            color: "var(--dark-red)",
         },
         transition: {
             duration: 0.5
         }
     }
 
-    const buttonsVariants2 = {
-
+    const secondButton = {
         initial: {
             opacity: 0,
             y: -100
@@ -42,8 +41,7 @@ function Card() {
         }
     }
 
-    const cardVariants = {
-
+    const card = {
         initial: {
             x: -100,
             opacity: 0
@@ -58,9 +56,16 @@ function Card() {
         }
     }
     return (
-        <div>
-            <h3 className="text-center">CARD</h3>
-        </div>
+        <>
+            <motion.div variants={secondButton} initial="initial" whileInView="animate" className="box">
+                <motion.button variants={firstButton} animate={selected == 1 ? "selected" : "unselected"} onClick={() => setSelected(1)} type='button' className="button-menu">Primi</motion.button>
+                <motion.button variants={firstButton} animate={selected == 2 ? "selected" : "unselected"} onClick={() => setSelected(2)} type='button' className="button-menu">Secondi</motion.button>
+                <motion.button variants={firstButton} animate={selected == 3 ? "selected" : "unselected"} onClick={() => setSelected(3)} type='button' className="button-menu">Hamburger</motion.button>
+                <motion.button variants={firstButton} animate={selected == 4 ? "selected" : "unselected"} onClick={() => setSelected(4)} type='button' className="button-menu">Pizze</motion.button>
+                <motion.button variants={firstButton} animate={selected == 5 ? "selected" : "unselected"} onClick={() => setSelected(5)} type='button' className="button-menu">Desserts</motion.button>
+                <motion.button variants={firstButton} animate={selected == 6 ? "selected" : "unselected"} onClick={() => setSelected(6)} type='button' className="button-menu">Bevande</motion.button>
+            </motion.div>
+        </>
     )
 
 }
