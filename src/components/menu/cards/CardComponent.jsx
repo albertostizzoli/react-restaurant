173f8d1b5c02622a66483dad4040ea7ab9e12d19
albertostizzoli <albertostizzoli@yahoo.it>
2024-07-24 15:32:09 +0200

    create const of Card Component

--- src/components/menu/cards/CardComponent.jsx
+++ src/components/menu/cards/CardComponent.jsx
@@ -5,7 +5,59 @@ import { Switch, Case, Default } from "react-if";
 import './card.css';
 
 function Card() {
-    return(
+
+    const [selected, setSelected] = useState(1)
+    const [data, setData] = useState([])
+
+    useEffect(() => {
+        fetch('src/menu.json').then(response => response.json()).then(data => { setData(data) })
+    }, [])
+
+    const buttonsVariants = {
+        selected: {
+            backgroundColor: "var(--orange)",
+            color: "white"
+        },
+        unselected: {
+            backgroundColor: "var(--main)",
+            color: "var(--white)",
+        },
+        transition: {
+            duration: 0.5
+        }
+    }
+
+    const buttonsVariants2 = {
+
+        initial: {
+            opacity: 0,
+            y: -100
+        },
+        animate: {
+            opacity: 1,
+            y: 0,
+            transition: {
+                duration: 0.5
+            }
+        }
+    }
+
+    const cardVariants = {
+
+        initial: {
+            x: -100,
+            opacity: 0
+        },
+        animate: {
+            x: 0,
+            opacity: 1,
+            transition: {
+                duration: 0.5,
+                staggerChildren: 0.1
+            }
+        }
+    }
+    return (
         <div>
             <h3 className="text-center">CARD</h3>
         </div>
